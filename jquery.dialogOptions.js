8f8b086d40d7ac31b5314a0ba2d1bfc4cd95ed38
Jason Day <movemaine@gmail.com>
2014-02-06 14:49:25 -0500

    Update jquery.dialogOptions.js
    New Options

--- jquery.dialogOptions.js
+++ jquery.dialogOptions.js
@@ -1,37 +1,40 @@
 /*
-* jQuery UI dialogOptions v1.0
-* @desc extending jQuery Ui Dialog - Responsive, click outside, class handling
-* @author Jason Day
-* 
-* Dependencies:
-*              jQuery: http://plugins.jquery.com/project/jPrintArea
-*              jQuery UI: https://github.com/permanenttourist/jquery.jqprint
-*              debouncedresize: https://github.com/louisremi/jquery-smartresize
-*
-* MIT license:
-*              http://www.opensource.org/licenses/mit-license.php
-*
-* (c) Jason Day 2014
-*
-* New Options:
-*	clickOut: true              // closes dialog when clicked outside
-*	responsive: true            // fluid width & height based on viewport
-*	showTitleBar: true          // hide titlebar
-*	showCloseButton: true       // hide close button
-*  
-* Added functionality:
-* 	add & remove dialogClass to .ui-widget-overlay for scoping styles
-* 	patch for: http://bugs.jqueryui.com/ticket/4671
-*/
-
-// orientation change event - requires debouncedresize
-var supportsOrientationChange = "onorientationchange" in window,
-    orientationEvent = supportsOrientationChange ? "orientationchange" : "debouncedresize";
-
+ * jQuery UI dialogOptions v1.0
+ * @desc extending jQuery Ui Dialog - Responsive, click outside, class handling
+ * @author Jason Day
+ *
+ * Dependencies:
+ *		jQuery: http://jquery.com/
+ *		jQuery UI: http://jqueryui.com/
+ *		Modernizr: http://modernizr.com/
+ *
+ * MIT license:
+ *              http://www.opensource.org/licenses/mit-license.php
+ *
+ * (c) Jason Day 2014
+ *
+ * New Options:
+ *	clickOut: true			// closes dialog when clicked outside
+ *	responsive: true		// fluid width & height based on viewport
+ *					        // true: always responsive
+ *					        // false: never responsive
+ *					        // "touch": only responsive on touch device
+ *  scaleH: 0.8             // responsive scale height percentage, 0.8 = 80% of viewport
+ *  scaleW: 0.8             // responsive scale width percentage, 0.8 = 80% of viewport
+ *	showTitleBar: true		// false: hide titlebar
+ *	showCloseButton: true	// false: hide close button
+ *
+ * Added functionality:
+ *  add & remove dialogClass to .ui-widget-overlay for scoping styles
+ *	patch for: http://bugs.jqueryui.com/ticket/4671
+ *	recenter dialog - ajax loaded content
+ */
 
 // add new options with default values
 $.ui.dialog.prototype.options.clickOut = true;
 $.ui.dialog.prototype.options.responsive = true;
+$.ui.dialog.prototype.options.scaleH = 0.8;
+$.ui.dialog.prototype.options.scaleW = 0.8;
 $.ui.dialog.prototype.options.showTitleBar = true;
 $.ui.dialog.prototype.options.showCloseButton = true;
 
@@ -40,45 +43,86 @@ $.ui.dialog.prototype.options.showCloseButton = true;
 var _init = $.ui.dialog.prototype._init;
 $.ui.dialog.prototype._init = function () {
     var self = this;
+
     // apply original arguments
     _init.apply(this, arguments);
+
+    //patch
+    $.ui.dialog.overlay.events = $.map('focus,keydown,keypress'.split(','), function (event) {
+        return event + '.dialog-overlay';
+    }).join(' ');
+
+};
+// end _init
+
+
+// extend open function
+var _open = $.ui.dialog.prototype.open;
+$.ui.dialog.prototype.open = function () {
+    var self = this;
+
+    // apply original arguments
+    _open.apply(this, arguments);
+
+    // get dialog original size on open
+    var oHeight = self.element.parent().outerHeight(),
+        oWidth = self.element.parent().outerWidth(),
+        isTouch = $("html").hasClass("touch");
+
     // responsive width & height
-    if (self.options["responsive"]) {
-         var resize = function () {
+    var resize = function () {
+
+        // check if responsive
+        // dependent on modernizr for device detection / html.touch
+        if (self.options.responsive === true || (self.options.responsive === "touch" && isTouch)) {
             var elem = self.element,
-                wHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight,
-                wWidth = "innerWidth" in window ? window.innerWidth : document.documentElement.offsetWidth,
+                wHeight = $(window).height(),
+                wWidth = $(window).width(),
                 dHeight = elem.parent().outerHeight(),
                 dWidth = elem.parent().outerWidth(),
-                setHeight = wHeight * 0.8,
-                setWidth = wWidth * 0.8;
+                setHeight = Math.min(wHeight * self.options.scaleH, oHeight),
+                setWidth = Math.min(wWidth * self.options.scaleW, oWidth);
             
-            if((dHeight + 100) > wHeight){
-                elem.dialog("option","height", setHeight);
-            }
-            if((dWidth + 100) > wWidth){
-                elem.dialog("option","width", setWidth);
+            // check & set height
+            if ((oHeight + 100) > wHeight || elem.hasClass("resizedH")) {
+                elem.dialog("option", "height", setHeight).parent().css("max-height", setHeight);
+                elem.addClass("resizedH");
             }
             
-            elem.dialog("option", "position", "center");
-           
-            elem.css({
-                "overflow-y": "scroll",
-                "-webkit-overflow-scrolling": "touch"
-            });
+            // check & set width
+            if ((oWidth + 100) > wWidth || elem.hasClass("resizedW")) {
+                elem.dialog("option", "width", setWidth).parent().css("max-width", setWidth);
+                elem.addClass("resizedW");
+            }
+
+            // only recenter & add overflow if dialog has been resized
+            if (elem.hasClass("resizedH") || elem.hasClass("resizedW")) {
+                elem.dialog("option", "position", "center");
+                elem.css("overflow", "auto");
+            }
         }
 
-        resize();
+        // add webkit scrolling to all dialogs for touch devices
+        if (isTouch) {
+            elem.css("-webkit-overflow-scrolling", "touch");
+        }
+    };
 
-        $(window).on(orientationEvent, function () {
-            resize(function () {
-                self.element.dialog("option", "position", "center");
-            });
-        });
-    }
+    // call resize()
+    resize();
+
+    // resize on window resize
+    $(window).on("resize", function () {
+        resize();
+    });
+
+    // resize on orientation change
+    window.addEventListener("orientationchange", function () {
+        resize();
+    });
 
     // hide titlebar
-    if (!self.options["showTitleBar"]) {
+    if (!self.options.showTitleBar) {
         self.uiDialogTitlebar.css({
             "height": 0,
             "padding": 0,
@@ -89,25 +133,12 @@ $.ui.dialog.prototype._init = function () {
     }
 
     //hide close button
-    if (!self.options["showCloseButton"]) {
+    if (!self.options.showCloseButton) {
         self.uiDialogTitlebar.find(".ui-dialog-titlebar-close").css("display", "none");
     }
-    
-    //patch
-    $.ui.dialog.overlay.events = $.map('focus,keydown,keypress'.split(','), function(event) { return event + '.dialog-overlay'; }).join(' ');
-
-}
-// end _init
-
-// extend open function
-var _open = $.ui.dialog.prototype.open;
-$.ui.dialog.prototype.open = function () {
-    var self = this;
-    // apply original arguments
-    _open.apply(this, arguments);
 
     // close on clickOut
-    if (self.options['clickOut'] && !self.options['modal']) {
+    if (self.options.clickOut && !self.options.modal) {
         // use transparent div - simplest approach (rework)
         $('<div id="dialog-overlay"></div>').insertBefore(self.element.parent());
         $('#dialog-overlay').css({
@@ -124,18 +155,19 @@ $.ui.dialog.prototype.open = function () {
             self.close();
         });
         // else close on modal click
-    } else if (self.options['modal']) {
+    } else if (self.options.clickOut && self.options.modal) {
         $('.ui-widget-overlay').click(function (e) {
             self.close();
         });
     }
 
     // add dialogClass to overlay
-    if (self.options['dialogClass']) {
-        $('.ui-widget-overlay').addClass(self.options['dialogClass']);
+    if (self.options.dialogClass) {
+        $('.ui-widget-overlay').addClass(self.options.dialogClass);
     }
-}
+};
 //end open
+
 
 // extend close function
 var _close = $.ui.dialog.prototype.close;
@@ -145,12 +177,12 @@ $.ui.dialog.prototype.close = function () {
     _close.apply(this, arguments);
 
     // remove dialogClass to overlay
-    if (self.options['dialogClass']) {
-        $('.ui-widget-overlay').removeClass(self.options['dialogClass']);
+    if (self.options.dialogClass) {
+        $('.ui-widget-overlay').removeClass(self.options.dialogClass);
     }
-     //remove clickOut overlay
-    if($("#dialog-overlay").length ){
+    //remove clickOut overlay
+    if ($("#dialog-overlay").length) {
         $("#dialog-overlay").remove();
     }
-}
+};
 //end close
